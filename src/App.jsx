ad20636595ac0ec20f941b5ecb40b2c8cbe1e440
Rakib <rakib@Rakibs-MacBook-Pro.local>
2025-08-19 22:54:47 +0600

    basic rounting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RegistrationPage from "./pages/RegistrationPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import NotFoundPage from "./pages/NotFoundPage";
+
 // src/App.jsx
 export default function App() {
   return (
-    <div className="flex-center min-h-screen">
-      <div className="card text-center">
-        <h1 className="text-2xl text-lwsGreen font-inter">Hello Tailwind v4 🚀</h1>
-        <button className="btn-primary mt-4">Click Me</button>
-      </div>
-    </div>
+    <>
+    <Routes>
+      <Route element={<HomePage/>} path="/" />
+      <Route element={<LoginPage/>} path="/login" />
+      <Route element={<RegistrationPage/>} path="/register" />
+      <Route element={<ProfilePage/>} path="/me" />
+      <Route element={<NotFoundPage/>} path="*" />
+
+    </Routes>
+    </>
   );
 }
