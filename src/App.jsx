ba903b85ea1a0352f05438753f478b1ca5462f3e
Rakib <rakib@Rakibs-MacBook-Pro.local>
2025-08-19 21:38:29 +0600

    a social platform basic setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+export default function App() {
+  return (
+    <div className="flex-center min-h-screen">
+      <div className="card text-center">
+        <h1 className="text-2xl text-lwsGreen font-inter">Hello Tailwind v4 🚀</h1>
+        <button className="btn-primary mt-4">Click Me</button>
+      </div>
+    </div>
+  );
+}
