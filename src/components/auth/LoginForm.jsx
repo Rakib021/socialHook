8191fb5989212248889d691b5c718c97f3a78c11
Rakib <rakib@Rakibs-MacBook-Pro.local>
2025-08-21 11:15:50 +0600

    useform and fake login and logout

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import Field from "../common/Field";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginForm() {
+
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+
+  const submitForm = (formData) => {
+    console.log(formData);
+    navigate('/');
+  };
   return (
-    <div >LoginForm</div>
-  )
+    <form
+      className="border-b border-[#3F3F3F] PB-10 LG:PB-[60PX"
+      onSubmit={handleSubmit(submitForm)}
+    >
+      <Field label="Email" error={errors.email}>
+        <input
+          {...register("email", { required: "Email ID is required" })}
+          className={`auth-input ${
+            errors.email ? "border-red-500" : "border-gray-400"
+          }`}
+          type="email"
+          name="email"
+          id="email"
+        />
+      </Field>
+      <Field label="Password" error={errors.password}>
+        <input
+          {...register("password", {
+            required: "Password  is required",
+            minLength: {
+              value: 8,
+              message: "Your password must be at least 8 characters",
+            },
+          })}
+          className={`auth-input ${
+            errors.password ? "border-red-600" : "border-gray-100"
+          }`}
+          type="password"
+          name="password"
+          id="password"
+        />
+      </Field>
+
+      <Field>
+        <button
+          className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+          type="submit"
+        >
+          Login
+        </button>
+      </Field>
+    </form>
+  );
 }
